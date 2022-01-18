fe5b452b253e7bec306736345213b5a19db77cbe
Berenguer Pou <berenguer.pou@gmail.com>
2022-01-18 19:47:14 +0100

    cases 4 and 5 described and passed OK.

--- strictEquals.test.js
+++ strictEquals.test.js
@@ -42,4 +42,30 @@ describe("Given a strictEquals function", () => {
       expect(areEqualValues).toBe(expectedResult);
     });
   });
+  describe("When it receives false as first value and false as second value", () => {
+    test("Then it should return true", () => {
+      // Arrange
+      const values = [false, false];
+      const expectedResult = true;
+
+      // Act
+      const areEqualValues = strictEquals(values);
+
+      // Assert
+      expect(areEqualValues).toBe(expectedResult);
+    });
+  });
+  describe("When it receives 'Water' as first value and 'oil' as second value", () => {
+    test("Then it should return false", () => {
+      // Arrange
+      const values = ["Water", "oil"];
+      const expectedResult = false;
+
+      // Act
+      const areEqualValues = strictEquals(values);
+
+      // Assert
+      expect(areEqualValues).toBe(expectedResult);
+    });
+  });
 });
